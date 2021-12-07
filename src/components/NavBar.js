153fc3925f404013e55860048620f1287ad57c31
Alejo Calandra <azueel@hotmail.com>
2021-12-06 21:31:36 -0300

    desafio2

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,5 +1,16 @@
 import React from 'react';
 
 export const NavBar = () => {
-	return <div></div>;
+	return (
+		<div>
+			<nav className="bgNav">
+				<div className="containerNav">
+					<a href="/">Home</a>
+					<a href="/">Productos</a>
+					<a href="/">Nosotros</a>
+					<a href="/">Contactanos</a>
+				</div>
+			</nav>
+		</div>
+	);
 };
